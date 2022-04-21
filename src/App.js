55088f4f0b18c446e2da5310b79b5dc0921a6d8a
Maurizio <mauriziodm91@gmail.com>
2022-04-21 17:43:27 -0300

    created components file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+
+import Cardlist from './components/card-list/card-list.component'
 
 class App extends Component {
   constructor() {
     super() //super calls the underline constructor method in Component
     this.state = {
       monsters: [],
+      searchField: '',
     } //instantiate the state its always a json object
   }
 
@@ -14,27 +16,37 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users') //returning a promise
       .then((response) => response.json())
       .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users }
-          },
-          () => {
-            console.log(this.state)
-          }
-        )
+        this.setState(() => {
+          return { monsters: users }
+        })
       )
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
   render() {
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField) //New Array with filtered results
+    })
+
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          )
-        })}
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={onSearchChange}
+        />
+        <Cardlist monsters={filteredMonsters} />
       </div>
     )
   }
